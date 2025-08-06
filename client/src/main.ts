d94ed1e0e8b335912fda18df56512ef0c08a66af
Nickson Ogm <nicksonogm@gmail.com>
2025-08-06 12:16:30 +0300

    almost done with google registration

--- client/src/main.ts
+++ client/src/main.ts
@@ -9,7 +9,7 @@ import GAuth from 'vue-google-oauth2'
 
 const app = createApp(App)
 const gauthOption = {
-  clientId: 'YOUR_GOOGLE_CLIENT_ID_HERE', // Use your client ID
+  clientId: '1088773727270-h7t9gs2vsg0iqokiuh9pn172gqt6001p.apps.googleusercontent.com', // Use your client ID
   scope: 'profile email',
   prompt: 'consent',
   fetch_basic_profile: true
