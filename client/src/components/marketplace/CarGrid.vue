8b0d77a0c37976bdbcbc1c3c14b48db68f786110
Arnold <arnoldqym@gmail.com>
2025-08-12 17:40:55 +0300

    added code for car grid and rentalstatsbar

---
+++ client/src/components/marketplace/CarGrid.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+    <CarCard v-for="car in cars" :key="car.id" :car="car" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import CarCard from './CarCard.vue'
+const cars = [/* mock or fetched rental data */]
+</script>
