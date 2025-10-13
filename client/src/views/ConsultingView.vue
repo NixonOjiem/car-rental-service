9f0d4a08075433aa965ab269db6effd484c233c9
Nickson Ogm <nicksonogm@gmail.com>
2025-10-13 13:33:44 +0300

    added basic layout components to the consulting page

--- client/src/views/ConsultingView.vue
+++ client/src/views/ConsultingView.vue
@@ -1,11 +1,14 @@
 <template lang="">
   <div>
+    <NavbarComponent />
 
+    <FooterComponent />
   </div>
 </template>
 
 <script setup lang="ts">
-
+import NavbarComponent from '../components/layouts/NavbarComponent.vue';
+import FooterComponent from '../components/layouts/FooterComponent.vue';
 </script>
 
 <style lang="">
